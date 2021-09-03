c0a410ea44a6524ea082609dbf43fcc258dbb23d
Koryagin Sergey <>
2021-09-03 18:05:56 +0300

    add something

---
+++ LandingPageSwiftUI/LandingPageSwiftUI/LoginView.swift
@@ -0,0 +1,27 @@
+//
+//  ContentView.swift
+//  LandingPageSwiftUI
+//
+//  Created by macuser on 03.09.2021.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+      Text("Hello").padding()
+      
+      
+//      VStack {
+//        Spacer()
+//        Text("Hello World!")
+//        Spacer()
+//      }.background(self)
+    }
+}
+
+struct LoginView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginView()
+    }
+}
