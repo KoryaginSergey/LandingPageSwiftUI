dfe6fc977a415c03f94dc00e960db8b895bbc553
Koryagin Sergey <>
2021-09-03 16:50:09 +0300

    start project

---
+++ LandingPageSwiftUI/LandingPageSwiftUI/LandingPageSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandingPageSwiftUIApp.swift
+//  LandingPageSwiftUI
+//
+//  Created by macuser on 03.09.2021.
+//
+
+import SwiftUI
+
+@main
+struct LandingPageSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
