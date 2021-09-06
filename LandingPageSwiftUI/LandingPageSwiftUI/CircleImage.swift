fb1c5c10381ec7dc70987db53e7cda0dbacd075a
Koryagin Sergey <>
2021-09-06 11:27:19 +0300

    added Map

---
+++ LandingPageSwiftUI/LandingPageSwiftUI/CircleImage.swift
@@ -0,0 +1,23 @@
+//
+//  CircleImage.swift
+//  LandingPageSwiftUI
+//
+//  Created by Admin on 05.09.2021.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Image("Roses")
+            .clipShape(Circle())
+            .overlay(Circle().stroke(Color.white, lineWidth: 4))
+            .shadow(radius: 7)
+    }
+}
+
+struct CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage()
+    }
+}
--- LandingPageSwiftUI/LandingPageSwiftUI/ContentView.swift
+++ LandingPageSwiftUI/LandingPageSwiftUI/ContentView.swift
@@ -9,8 +9,36 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
+        VStack {
+            MapView()
+                .ignoresSafeArea(edges: .top)
+                .frame(height: 300)
+            
+            CircleImage()
+                .offset(y: -230)
+                .padding(.bottom, -130)
+            
+            VStack(alignment: .leading) {
+                Text("Hello, world!")
+                    .font(.title)
+                HStack {
+                    Text("HI bro!")
+                    Spacer()
+                    Text("How are you?")
+                }
+                .font(.subheadline)
+                .foregroundColor(.secondary)
+                
+                Divider()
+
+                Text("About Turtle Rock")
+                    .font(.title2)
+                Text("Descriptive text goes here.")
+            }
             .padding()
+            
+            Spacer()
+        }
     }
 }
 
